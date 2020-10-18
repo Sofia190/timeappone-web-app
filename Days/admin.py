d4084c19e3c8b6832d872c733912c5f764479468
Diaconescu Maria Raluca <44669618+Sofia190@users.noreply.github.com>
2020-10-18 19:40:23 +0300

    Added Days app

---
+++ Days/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Day, Name, NameModel, DaysInSearchEngine
+
+
+admin.site.register(Day)
+
+admin.site.register(Name)
+
+admin.site.register(NameModel)
+
+admin.site.register(DaysInSearchEngine)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
